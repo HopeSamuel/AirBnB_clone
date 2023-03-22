d140778e6f1b63493a0fdbb06d64377b662b783c
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-03-22 20:17:50 +0100

    Pp

--- models/place.py
+++ models/place.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Module for Place class."""
-
+"""
+Place Representation module
+"""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Class representing a Place."""
+    """
+    Represent a Place
+    Inherit from base model
+    """
+
     city_id = ""
     user_id = ""
     name = ""
@@ -16,4 +21,4 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = []
+    amenity_ids = list()
