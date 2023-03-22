96a5ddf30f21b07be3e5e39195ad7b1d9248e231
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-03-22 20:11:51 +0100

    In

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
 
+#!/usr/bin/python3
+"""
+"""
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
