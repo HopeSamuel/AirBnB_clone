eaad5bccd3ede386401d778c10c24dcf2beaf7e4
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-04-21 18:20:17 +0100

    Bb

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,7 @@
-Hope Samuel 
+#!/usr/bin/python3
+"""Create a unique FileStorage instance for your application"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
